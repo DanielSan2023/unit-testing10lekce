9ed60abedae6734a2796e624e534c8b7999e7b0a
Daniel_San <electronic.san@gmail.com>
2023-11-26 15:27:27 +0100

    added Calc class

--- src/main/java/com/engeto/Main.java
+++ src/main/java/com/engeto/Main.java
@@ -1,7 +1,9 @@
 package com.engeto;
 
+import com.engeto.calculation.Calc;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello World:");
     }
 }
--- src/main/java/com/engeto/calculation/Calc.java
+++ src/main/java/com/engeto/calculation/Calc.java
@@ -1,4 +1,8 @@
 package com.engeto.calculation;
 
 public class Calc {
+    public static int scitani(int a,int b) {
+        return a + b;
+
+    }
 }
